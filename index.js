197c96babac37ec0ce47581369ca8aea504e437d
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-09-10 16:36:19 +0300

    complete

---
+++ index.js
@@ -0,0 +1,20 @@
+function Scooter(year,color,model){
+    this.year = year;
+    this.color = color;
+    this.model = model;
+}
+const newScooter = new Scooter(2022,"blue","Audi")
+
+function Driver(name,age,experience){
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+}
+const newDriver = new Driver("blue",27 ,"Good")
+
+
+function PickupLocation(address, city){
+    this.address = address;
+    this.city = city;
+}
+const newPickupLocation = new pickUpLocation(200216,"South B")
